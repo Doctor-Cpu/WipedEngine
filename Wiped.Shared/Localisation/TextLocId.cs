068aa5e1a6ec0f5b975d677d224a4461ad515407
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2026-02-09 12:25:09 +0000

    text localisation id

---
+++ Wiped.Shared/Localisation/TextLocId.cs
@@ -0,0 +1,31 @@
+namespace Wiped.Shared.Localisation;
+
+public readonly record struct TextLocId(string Id) : IEquatable<string>, IComparable<TextLocId>
+{
+    public static implicit operator string(TextLocId locId)
+    {
+        return locId.Id;
+    }
+
+    public static implicit operator TextLocId(string id)
+    {
+        return new TextLocId(id);
+    }
+
+    public static implicit operator TextLocId?(string? id)
+    {
+        return id == null ? default(TextLocId?) : new TextLocId(id);
+    }
+
+    public bool Equals(string? other)
+    {
+        return Id == other;
+    }
+
+    public int CompareTo(TextLocId other)
+    {
+        return string.Compare(Id, other.Id, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public override string ToString() => Id ?? throw new InvalidOperationException("LocId is uninitialized");
+}
