1c5c1f9110a4634bebed5052699869f456139dae
Doctor-Cpu <77215380+Doctor-Cpu@users.noreply.github.com>
2026-02-28 12:25:22 +0000

    nuke yml

--- Wiped.Shared/Serialization/DataDefinitionAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace Wiped.Shared.Serialization;
-
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
-public sealed class DataDefinitionAttribute(string? name = null) : Attribute
-{
-	public string? Name { get; } = name;
-}
